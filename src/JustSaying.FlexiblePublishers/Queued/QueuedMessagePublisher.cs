d6b9061354362b54b966f9504104a6b66229eb2c
Mike Goodfellow <mike@audiu.net>
2019-06-15 09:28:24 +0100

    CHANGED: Add logging

--- src/JustSaying.FlexiblePublishers/Queued/QueuedMessagePublisher.cs
+++ src/JustSaying.FlexiblePublishers/Queued/QueuedMessagePublisher.cs
@@ -4,18 +4,22 @@ using System.Threading;
 using System.Threading.Tasks;
 using JustSaying.Messaging;
 using JustSaying.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace JustSaying.FlexiblePublishers.Queued
 {
     public class QueuedMessagePublisher : IQueuedMessagePublisher
     {
+        private readonly ILogger<IQueuedMessagePublisher> _logger;
         private readonly IMessagePublisher _messagePublisher;
 
         private readonly Queue<MessageContainer> _queuedMessages = new Queue<MessageContainer>();
 
-        public QueuedMessagePublisher(IMessagePublisher messagePublisher)
+        public QueuedMessagePublisher(ILoggerFactory loggerFactory, IMessagePublisher messagePublisher)
         {
             _messagePublisher = messagePublisher;
+            _logger = loggerFactory.CreateLogger<QueuedMessagePublisher>();
         }
 
         public Task PublishAsync(Message message)
@@ -68,52 +72,56 @@ namespace JustSaying.FlexiblePublishers.Queued
         {
             var tracer = Guid.NewGuid();
 
-            if (QueuedItems == 0)
+            using (_logger.BeginScope(new Dictionary<string, object> {{"tracer", tracer.ToString()}}))
             {
-                //_logger.Debug("Trace {traceId}: No queued messages", tracer);
 
-                return;
-            }
-
-            //_logger.Information(
-            //    "Trace {traceId}: Sending {messageCount} queued messages",
-            //    tracer,
-            //    _queuedMessages.Count);
-
-            while (QueuedItems > 0)
-            {
-                var container = _queuedMessages.Dequeue();
-
-                if (!onlySendWhitelisted || container.IsWhitelisted)
+                if (QueuedItems == 0)
                 {
-                    try
-                    {
-                        await _messagePublisher.PublishAsync(container.Message, cancellationToken);
+                    _logger.LogDebug("No queued messages");
 
-                        //_logger.Information(
-                        //    "Posted message to SQS - Queue: {queueUrl}, MessageBody: {messageBody}",
-                        //    container.SendMessageRequest.QueueUrl,
-                        //    container.SendMessageRequest.MessageBody);
-                    }
-                    catch (Exception ex)
-                    {
-                        //_logger.Error(
-                        //    ex,
-                        //    "Failed to post message to Queue: {queueUrl}, MessageBody: {messageBody}",
-                        //    container.SendMessageRequest.QueueUrl,
-                        //    container.SendMessageRequest.MessageBody);
-                    }
+                    return;
                 }
-                else
+
+                _logger.LogInformation($"Sending {_queuedMessages.Count} queued messages"); 
+
+                while (QueuedItems > 0)
                 {
-                    //_logger.Warning(
-                    //    "Dropping message as it is not whitelisted, and only sending whitelisted messages - Queue: {queueUrl}, MessageBody: {messageBody}",
-                    //    container.SendMessageRequest.QueueUrl,
-                    //    container.SendMessageRequest.MessageBody);
+                    var container = _queuedMessages.Dequeue();
+
+                    var messageType = container.Message.GetType().Name;
+
+                    using (_logger.BeginScope(new Dictionary<string, object>
+                    {
+                        { "isWhitelisted", container.IsWhitelisted },
+                        { "messageType", messageType },
+                        { "messageBody", JsonConvert.SerializeObject(container.Message) }
+                    }))
+                    {
+
+                        if (!onlySendWhitelisted || container.IsWhitelisted)
+                        {
+                            try
+                            {
+                                await _messagePublisher.PublishAsync(container.Message, cancellationToken);
+
+                                _logger.LogInformation($"Published message successfully - MessageType: {messageType}");
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogError(ex, $"Failed to post message - MessageType: {messageType}");
+                            }
+                        }
+                        else
+                        {
+                            _logger.LogWarning(
+                                $"Dropping message as it is not whitelisted, and only sending whitelisted messages - MessageType: {messageType}");
+                        }
+                    }
+
                 }
+
+                _logger.LogInformation("Finished sending queued messages");
             }
-          
-            //_logger.Information("Trace {traceId}: Finished sending queued messages", tracer);
         }
     }
 }
